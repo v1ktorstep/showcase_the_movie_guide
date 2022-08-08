e368f3a368c1cb5f4617906b1f293b947f6e9721
vs <smoke.ltd.vs@gmail.com>
2022-08-08 15:08:58 +0300

    Add main page.

---
+++ lib/features/main/presentation/main_page.dart
@@ -0,0 +1,84 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:showcase_the_movie_guide/core/routes/app_router.dart';
+import 'package:showcase_the_movie_guide/features/app/application/app_login/app_login_cubit.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsRouter(
+      routes: const [
+        MovieListRoute(),
+      ],
+      builder: (context, child, animation) {
+        final tabsRouter = AutoTabsRouter.of(context);
+
+        return Scaffold(
+          appBar: AppBar(
+            actions: const [
+              _AccountIconButton(),
+            ],
+          ),
+          body: FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: tabsRouter.activeIndex,
+            onTap: (index) {
+              tabsRouter.setActiveIndex(index);
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.local_movies_outlined),
+                label: 'Discover',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search_outlined),
+                label: 'Search',
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _AccountIconButton extends StatelessWidget {
+  const _AccountIconButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AppLoginCubit, AppLoginState>(
+      builder: (context, state) {
+        if (state.loggedIn) {
+          final accountDetails = state.accountDetails;
+
+          final icon = accountDetails == null
+              ? const Icon(Icons.account_circle_rounded)
+              : CachedNetworkImage(
+                  imageUrl:
+                      'https://www.gravatar.com/avatar/${accountDetails.avatar.gravatar.hash}',
+                );
+
+          return IconButton(
+            onPressed: () {},
+            icon: icon,
+          );
+        } else {
+          return IconButton(
+            onPressed: () {
+              context.router.push(const SignInRoute());
+            },
+            icon: const Icon(Icons.account_circle_rounded),
+          );
+        }
+      },
+    );
+  }
+}
--- lib/features/movie_list/presentation/movie_list_page.dart
+++ lib/features/movie_list/presentation/movie_list_page.dart
@@ -2,15 +2,12 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:showcase_the_movie_guide/core/di/injectable.dart';
-import 'package:showcase_the_movie_guide/core/routes/app_router.dart';
-import 'package:showcase_the_movie_guide/features/app/application/app_login/app_login_cubit.dart';
 import 'package:showcase_the_movie_guide/features/movie_list/application/item_source.dart';
 import 'package:showcase_the_movie_guide/features/movie_list/application/movie_list_bloc.dart';
 import 'package:showcase_the_movie_guide/features/movie_list/presentation/movie_category.dart';
 import 'package:showcase_the_movie_guide/features/movie_list/presentation/tv_category.dart';
 import 'package:showcase_the_movie_guide/features/tmdb/domain/entities/movie_page.dart';
 import 'package:showcase_the_movie_guide/features/tmdb/domain/entities/tv_page.dart';
-import 'package:auto_route/auto_route.dart';
 
 class MovieListPage extends StatelessWidget {
   const MovieListPage({Key? key}) : super(key: key);
@@ -21,30 +18,23 @@ class MovieListPage extends StatelessWidget {
       create: (context) {
         return getIt<MovieListBloc>()..add(const MovieListEvent.load());
       },
-      child: Scaffold(
-        appBar: AppBar(
-          actions: const [
-            _AccountIconButton(),
-          ],
-        ),
-        body: BlocBuilder<MovieListBloc, MovieListState>(
-          builder: (context, state) {
-            return state.when(
-              initial: () => const _MovieListLoading(),
-              loading: () => const _MovieListLoading(),
-              error: () => const _MovieListError(),
-              loaded: (
-                movieCategories,
-                tvCategories,
-              ) {
-                return _MovieList(
-                  movieCategories: movieCategories,
-                  tvCategories: tvCategories,
-                );
-              },
-            );
-          },
-        ),
+      child: BlocBuilder<MovieListBloc, MovieListState>(
+        builder: (context, state) {
+          return state.when(
+            initial: () => const _MovieListLoading(),
+            loading: () => const _MovieListLoading(),
+            error: () => const _MovieListError(),
+            loaded: (
+              movieCategories,
+              tvCategories,
+            ) {
+              return _MovieList(
+                movieCategories: movieCategories,
+                tvCategories: tvCategories,
+              );
+            },
+          );
+        },
       ),
     );
   }
@@ -92,40 +82,6 @@ class _MovieList extends StatelessWidget {
         } else {
           return TvCategory(
             itemSource: tvCategories[index - movieCategories.length],
-          );
-        }
-      },
-    );
-  }
-}
-
-class _AccountIconButton extends StatelessWidget {
-  const _AccountIconButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AppLoginCubit, AppLoginState>(
-      builder: (context, state) {
-        if (state.loggedIn) {
-          final accountDetails = state.accountDetails;
-
-          final icon = accountDetails == null
-              ? const Icon(Icons.account_circle_rounded)
-              : CachedNetworkImage(
-                  imageUrl:
-                      'https://www.gravatar.com/avatar/${accountDetails.avatar.gravatar.hash}',
-                );
-
-          return IconButton(
-            onPressed: () {},
-            icon: icon,
-          );
-        } else {
-          return IconButton(
-            onPressed: () {
-              context.router.push(const SignInRoute());
-            },
-            icon: const Icon(Icons.account_circle_rounded),
           );
         }
       },
